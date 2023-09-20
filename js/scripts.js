504c255491701588f5f9ec18f51238c9a4af2c50
Arum Yani <diniaditya2002@gmail.com>
2023-09-20 14:34:42 +0700

    update20/092023

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 let tasks = [];
 
 function addTask() {
-    const taskInput = document.getElementById('newTask');
-    const taskValue = taskInput.value.trim();
+  const taskInput = document.getElementById("newTask");
+  const taskValue = taskInput.value.trim();
 
-    if(taskValue) {
-        tasks.push(taskValue);
-        renderTasks();
-        taskInput.value = '';
-    }
+  if (taskValue) {
+    tasks.push(taskValue);
+    renderTasks();
+    taskInput.value = "";
+  }
 }
 
 function renderTasks() {
-    const list = document.getElementById('todoList');
-    list.innerHTML = '';
-    tasks.forEach((task, index) => {
-        list.innerHTML += `
+  const list = document.getElementById("todoList");
+  list.innerHTML = "";
+  tasks.forEach((task, index) => {
+    list.innerHTML += `
             <li>
                 ${task}
                 <input type="text" id="edit-${index}" value="${task}" style="display: none;">
@@ -25,29 +25,28 @@ function renderTasks() {
                     <button onclick="removeTask(${index})" id="delete">Hapus</button>
                 </div>
             </li>`;
-    });
+  });
 }
 
 function removeTask(index) {
-    tasks.splice(index, 1);
-    renderTasks();
+  tasks.splice(index, 1);
+  renderTasks();
 }
 
 function editTask(index) {
-    document.getElementById(`edit-${index}`).style.display = 'inline-block';
-    document.getElementById(`save-${index}`).style.display = 'inline-block';
+  document.getElementById(`edit-${index}`).style.display = "inline-block";
+  document.getElementById(`save-${index}`).style.display = "inline-block";
 }
 
 function saveTask(index) {
-    const editedTask = document.getElementById(`edit-${index}`).value.trim();
+  const editedTask = document.getElementById(`edit-${index}`).value.trim();
 
-    if(editedTask) {
-        tasks[index] = editedTask;
-        renderTasks();
-    } else {
-        alert('Task tidak boleh kosong!');
-    }
+  if (editedTask) {
+    tasks[index] = editedTask;
+    renderTasks();
+  } else {
+    alert("Task tidak boleh kosong!");
+  }
 }
-
 
 renderTasks();
